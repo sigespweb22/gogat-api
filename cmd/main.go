7b6c8604c4276b1fbb70346f9023e8ed8ac569d4
Alan Leite de Rezende <alanleitederezende@Alans-MacBook-Pro.local>
2024-08-14 10:23:52 -0300

    feat: more implementations

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+// "github.com/sigespweb22/gogate-api/internal/middlewares"
+// "github.com/sigespweb22/gogate-api/internal/proxy"
+// "github.com/sigespweb22/gogate-api/internal/server"
+// "github.com/go-chi/chi/v5/middleware"
+
+func main() {
+	// server := server.NewServer()
+}
---
+++ internal/proxy/proxy.go
@@ -0,0 +1,11 @@
+func TodoAPIProxy() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		url, err := url.Parse(configs.GetConfig().Service.TODO_ENDPOINT)
+		if err != nil {
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			return
+		}
+		httpProxy := httputil.NewSingleHostReverseProxy(url)
+		httpProxy.ServeHTTP(w, r)
+	})
+}
